4bbe25ad7dcbd9fc0818b66e126d4dccd0962976
Zaza.codes <jovienloba1@gmail.com>
2023-05-30 11:54:39 +0100

    update: college controller deserialize
    Signed-off-by: Zaza.codes <jovienloba1@gmail.com>

--- controllers/college.controller.go
+++ controllers/college.controller.go
@@ -1,14 +1,12 @@
 package controllers
 
 import (
-	"fmt"
-	"github.com/JoelOvien/cbt-backend/database"
+	"github.com/JoelOvien/cbt-backend/middleware"
 	"github.com/JoelOvien/cbt-backend/models"
-	"github.com/JoelOvien/cbt-backend/utils"
+
 	"github.com/gofiber/fiber/v2"
 	"gorm.io/gorm"
 	"strconv"
-	"strings"
 )
 
 // CollegeController is a struct to store our db instance
@@ -33,58 +31,7 @@ func (cc *CollegeController) FetchAllColleges(ctx *fiber.Ctx) error {
 
 	var colleges []models.College
 
-	// if user token from Authorization header is wrong from the DesertializeUser middleware, return error
-	var accessToken string
-	cookie := new(fiber.Cookie)
-	cookie.Name = "access_token"
-
-	authorizationHeader := ctx.Get("Authorization")
-	fmt.Println(authorizationHeader)
-
-	fields := strings.Fields(authorizationHeader)
-
-	if len(fields) != 0 && fields[0] == "Bearer" {
-		accessToken = fields[1]
-		fmt.Println(accessToken)
-	} else {
-		accessToken = cookie.Value
-		fmt.Println(accessToken)
-	}
-
-	if accessToken == "" {
-		fmt.Println("error access token is empty stirng")
-		return ctx.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
-			"status":  fiber.StatusUnauthorized,
-			"message": "You are not logged in",
-		})
-	}
-
-	config, _ := database.LoadConfig(".")
-	fmt.Println("after setting config", accessToken)
-
-	//called this function to validate the access token and extract the payload (user’s ID) we stored in it
-	sub, err := utils.ValidateToken(accessToken, config.AccessTokenPublicKey)
-	if err != nil {
-		fmt.Println("error", err.Error())
-		return ctx.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
-			"status":  fiber.StatusUnauthorized,
-			"message": err.Error(),
-		})
-
-	}
-
-	var user models.Users
-	userResult := database.DB.Table("USERS").First(&user, "UserID = ?", fmt.Sprint(sub))
-	if userResult.Error != nil {
-		fmt.Println("error", userResult.Error)
-		return ctx.Status(fiber.StatusForbidden).JSON(fiber.Map{
-			"status":  fiber.StatusForbidden,
-			"message": "The user belonging to this token no logger exists",
-		})
-
-	}
-
-	ctx.Set("currentUser", user.UserID)
+	middleware.DeserializeUser(ctx)
 
 	// get all colleges in COLLEGE table
 	result := cc.DB.Table("COLLEGE").Limit(intLimit).Offset(offset).Find(&colleges)
